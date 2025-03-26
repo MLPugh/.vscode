db22fae2b3326def405f6b7417d20f275e4232a7
Matthew <matthewleepugh@gmail.com>
2025-03-26 00:11:08 -0400

    added more LocalStorage

--- check.js
+++ check.js
@@ -1,14 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const checkbox = document.getElementById("main-one");
+    const mainOneCheck = document.getElementById("main-one");
+    const mainTwoCheck = document.getElementById("main-two");
     
     // Load saved state from Local Storage
     if (localStorage.getItem("checkboxState") === "checked") {
-        checkbox.checked = true;
+        mainOneCheck.checked = true;
+        mainTwoCheck.checked = true;
     }
 
     // Listen for changes and update Local Storage
-    checkbox.addEventListener("change", function () {
-        if (checkbox.checked) {
+    mainOneCheck.addEventListener("change", function () {
+        if (mainOneCheck.checked) {
+            localStorage.setItem("checkboxState", "checked");
+        } else {
+            localStorage.setItem("checkboxState", "unchecked");
+        }
+    });
+
+    mainTwoCheck.addEventListener("change", function () {
+        if (mainTwoCheck.checked) {
             localStorage.setItem("checkboxState", "checked");
         } else {
             localStorage.setItem("checkboxState", "unchecked");

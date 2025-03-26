60755c61d3d237074371889b6545c608464659a2
Matthew <matthewleepugh@gmail.com>
2025-03-26 00:44:34 -0400

    interactivity

--- check.js
+++ check.js
@@ -13,4 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
             localStorage.setItem(`checkboxState_${checkboxId}`, checkbox.checked ? "checked" : "unchecked");
         });
     });
+
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
 });

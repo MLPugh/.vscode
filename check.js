f69abe0164c9579bc632499d9c4e10b178f317be
Matthew <matthewleepugh@gmail.com>
2025-03-26 00:07:16 -0400

    removed tests

--- check.js
+++ check.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const checkbox = document.getElementById("rememberMe");
-
-    console.log('DOM content loaded');
+    const checkbox = document.getElementById("main-one");
     
     // Load saved state from Local Storage
     if (localStorage.getItem("checkboxState") === "checked") {
         checkbox.checked = true;
-        alert('remember checked')
     }
 
     // Listen for changes and update Local Storage
     checkbox.addEventListener("change", function () {
         if (checkbox.checked) {
             localStorage.setItem("checkboxState", "checked");
-            alert('checked');
         } else {
             localStorage.setItem("checkboxState", "unchecked");
-            alert('unchecked');
         }
     });
 });

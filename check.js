d7c0ff944890dcbe52ff1c1a92cce5a5d7d0a172
Matthew <matthewleepugh@gmail.com>
2025-03-28 15:55:11 -0400

    added tab system for regions

--- check.js
+++ check.js
@@ -1,31 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+  const checkboxes = document.querySelectorAll("input[type='checkbox']");
 
-    checkboxes.forEach(checkbox => {
-        const checkboxId = checkbox.id;
-        const savedState = localStorage.getItem(`checkboxState_${checkboxId}`);
+  checkboxes.forEach(checkbox => {
+    const checkboxId = checkbox.id;
+    const savedState = localStorage.getItem(`checkboxState_${checkboxId}`);
 
-        if (savedState === "checked") {
-            checkbox.checked = true;
-        }
-
-        checkbox.addEventListener("change", function () {
-            localStorage.setItem(`checkboxState_${checkboxId}`, checkbox.checked ? "checked" : "unchecked");
-        });
-    });
-
-    var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+    if (savedState === "checked") {
+      checkbox.checked = true;
     }
+
+    checkbox.addEventListener("change", function () {
+      localStorage.setItem(`checkboxState_${checkboxId}`, checkbox.checked ? "checked" : "unchecked");
+      });
   });
-}
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+    
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
 });
